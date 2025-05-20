885052a40687bb2b9f3f06defb724abfe2839a5c
JuanRdBo <juan@decaf.so>
2025-05-20 11:05:17 -0600

    repro expo53

--- index.js
+++ index.js
@@ -3,8 +3,4 @@ import "react-native-get-random-values";
 import { install } from "react-native-quick-crypto";
 install();
 
-// To make inactive screens be suspended from re-rendering
-import { enableFreeze } from "react-native-screens";
-enableFreeze(true);
-
 import "expo-router/entry";
